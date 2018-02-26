3fc2045dd5e719a4ae04d0f2b54100515147cb7d
AddOn2394 <jose_23marzo@outlook.com>
2018-02-26 14:12:02 -0600

    añadir temporizador

--- js/app.js
+++ js/app.js
@@ -12,11 +12,21 @@ $(document).ready(function () {
 
     setInterval(cambiarColor, 500);
 
-    for (var i = 1; i < 11; i++) {
+    for (var i = 1; i < 8; i++) {
         for (var b = 1; b < 8; b++) {
             $('.col-' + b).prepend('<img style="width : 100%;" src="image/' + (1 + Math.floor(Math.random() * 4)) + '.png">')
             console.log()
         }
-    }         
+    }
+    
+    $(".btn-reinicio").click(function () {
+        if ($(".btn-reinicio").text()=='Iniciar') {
+            $(".btn-reinicio").text("Reiniciar");
+        }
+        else{
+            $(".btn-reinicio").text("Iniciar");
+        }
+        ;
+    });
 });
 
