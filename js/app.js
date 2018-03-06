edc933c64d1ff24eb207830dbd523a181f001766
AddOn2394 <jose_23marzo@outlook.com>
2018-03-06 07:34:00 -0600

    Revert "Terminar parte del drag"
    This reverts commit dd533ab3d4840623fd465bcfdf6059a4d67d27c7.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
 
-    /*$('.panel-tablero').click(function (e) {
-        alert(e.target.id);
-        // ...
+    $(".main-titulo").click(function () {
+        
     });
-*/
+
     function cambiarColor() {
 
         $('.main-titulo').animate({
@@ -16,19 +15,13 @@ $(document).ready(function () {
     }
 
     setInterval(cambiarColor, 500);
-    var c = 0;
+
     for (var i = 1; i < 8; i++) {
         for (var b = 1; b < 8; b++) {
-            $('.col-' + b).prepend('<img class="draggable" style="width : 100%; " src="image/' + (1 + Math.floor(Math.random() * 4)) + '.png" id="' + c + '">')
-            c++;
+            $('.col-' + b).prepend('<img style="width : 100%; " src="image/' + (1 + Math.floor(Math.random() * 4)) + '.png">')
+            console.log()
         }
     }
     
-//    $(".draggable").draggable({ grid: [90, 90] });
-    
-    $("#sortable1, #sortable2, #sortable3, #sortable4, #sortable5, #sortable6, #sortable6").sortable({
-        connectWith: ".connectedSortable"
-    }).disableSelection();
-
 });
 
