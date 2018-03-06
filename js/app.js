e61b0677c2f39cfe9b9ef1b21cd25da6808015e8
AddOn2394 <jose_23marzo@outlook.com>
2018-03-06 07:33:00 -0600

    Revert "Finalización con correciontes"
    This reverts commit 630e4912b39d303aaf782bc4c8d7b96d7f451aec.

--- js/app.js
+++ js/app.js
@@ -1,105 +1,34 @@
 $(document).ready(function () {
-   function swapCandy(event, candyDrag) {
-        var candyDrag = $(candyDrag.draggable);
-        var dragSrc = candyDrag.attr('src');
-        var candyDrop = $(this);
-        var dropSrc = candyDrop.attr('src');
-        candyDrag.attr('src', dropSrc);
-        candyDrop.attr('src', dragSrc);
 
-        setTimeout(function () {
-            checkBoard();
-            if ($('img.delete').length === 0) {
-                candyDrag.attr('src', dragSrc);
-                candyDrop.attr('src', dropSrc);
-            } else {
-                updateMoves();
-            }
-        }, 500);
+    /*$('.panel-tablero').click(function (e) {
+        alert(e.target.id);
+        // ...
+    });
+*/
+    function cambiarColor() {
 
-        function updateMoves() {
-            var actualValue = Number($('#movimientos-text').text());
-            var result = actualValue += 1;
-            $('#movimientos-text').text(result);
-        }
+        $('.main-titulo').animate({
+            color: "#fff"
+        }, 100);
 
-        function checkBoard() {
-            fillBoard();
-        }
-
-        function fillBoard() {
-            var top = 6;
-            var column = $('[class^="col-"]');
-
-            column.each(function () {
-                var candys = $(this).children().length;
-                var agrega = top - candys;
-                for (var i = 0; i < agrega; i++) {
-                    var candyType = getRandomInt(1, 5);
-                    if (i === 0 && candys < 1) {
-                        $(this).append('<img src="image/' + candyType + '.png" class="element"></img>');
-                    } else {
-                        $(this).find('img:eq(0)').before('<img src="image/' + candyType + '.png" class="element"></img>');
-                    }
-                }
-            });
-            addCandyEvents();
-            setValidations();
-        }
-
-        function setValidations() {
-            columnValidation();
-            rowValidation();
-            if ($('img.delete').length !== 0) {
-                deletesCandyAnimation();
-            }
-        }
-
-        function addCandyEvents() {
-            $('img').draggable({
-                containment: '.panel-tablero',
-                droppable: 'img',
-                revert: true,
-                revertDuration: 500,
-                grid: [100, 100],
-                zIndex: 10,
-                drag: constrainCandyMovement
-            });
-            $('img').droppable({
-                drop: swapCandy
-            });
-            enableCandyEvents();
-        }
-
-        function disableCandyEvents() {
-            $('img').draggable('disable');
-            $('img').droppable('disable');
-        }
-
-        function enableCandyEvents() {
-            $('img').draggable('enable');
-            $('img').droppable('enable');
-        }
-        function deletesCandyAnimation() {
-            disableCandyEvents();
-            $('img.delete').effect('pulsate', 400);
-            $('img.delete').animate({
-                opacity: '0'
-            }, {
-                    duration: 300
-                })
-                .animate({
-                    opacity: '0'
-                }, {
-                        duration: 400,
-                        complete: function () {
-                            deletesCandy()
-                                .then(checkBoardPromise)
-                                .catch(showPromiseError);
-                        },
-                        queue: true
-                    });
-        }
+        $('.main-titulo').animate({color: "yellow"}, cambiarColor);
 
     }
+
+    setInterval(cambiarColor, 500);
+    var c = 0;
+    for (var i = 1; i < 8; i++) {
+        for (var b = 1; b < 8; b++) {
+            $('.col-' + b).prepend('<img class="draggable" style="width : 100%; " src="image/' + (1 + Math.floor(Math.random() * 4)) + '.png" id="' + c + '">')
+            c++;
+        }
+    }
+    
+//    $(".draggable").draggable({ grid: [90, 90] });
+    
+    $("#sortable1, #sortable2, #sortable3, #sortable4, #sortable5, #sortable6, #sortable6").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+
 });
+
